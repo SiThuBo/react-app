108008bbe549769dfe453e8c381443c578541543
SithuBo <sithubo.stb97@gmail.com>
2025-03-12 18:07:46 +0700

    children, reactnode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
-function App () {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => console.log(item);
-
-  return <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />;
-}
+const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert!</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        Click Me
+      </Button>
+    </div>
+  );
+};
 
 export default App;
